142e81b9fff9b30d6a0cabcef93fdfa35186aa85
IntlabSeJun <kimsejun5@naver.com>
2023-07-28 00:55:25 +0900

    bfs

--- 9. BFS/벽 부수고 이동하기(2206).cpp
+++ 9. BFS/벽 부수고 이동하기(2206).cpp
@@ -7,14 +7,15 @@
 #include<string>
 using namespace std;
 
-void print_map(vector<string>& map) {
-	for (int i = 0; i < map.size(); i++) {
-		for (int j = 0; j < map[0].size(); j++)
-			cout << map[i][j];
-		cout << "\n";
-	}
-	cout << "\n";
-}
+
+//void print_map(vector<vector<int>> &map) {
+//	for (int i = 0; i < map.size(); i++) {
+//		for (int j = 0; j < map[0].size(); j++)
+//			cout << map[i][j];
+//		cout << "\n";
+//	}
+//	cout << "\n";
+//}
 
 int main() {
 	ios::sync_with_stdio(0);
@@ -25,11 +26,15 @@ int main() {
 
 	vector<vector<int>> map_check(N, vector<int>(M));
 	vector<vector<int>> broken_map_check(N, vector<int>(M));
-	vector<string> map(N);
+	char map[1002][1002];
 
 	string s;
-	for (int i = 0; i < N; i++) cin >> map[i];
-
+	for (int i = 0; i < N; i++) {
+		cin >> s;
+		for (int j = 0; j < M; j++) {
+			map[i][j] = s[j];
+		}
+	}
 
 	map_check[0][0] = 1;
 	map_check[N - 1][M - 1] = 7777;
@@ -53,23 +58,30 @@ int main() {
 			p = Q.front().first;
 			broken = Q.front().second;
 			Q.pop();
+			//print_map(broken_map_check);
 			for (int i = 0; i < 4; i++) {
 				x = p.first + dx[i];
 				y = p.second + dy[i];
 				if (x < 0 || x >= N || y < 0 || y >= M) continue;
 				if (broken == 1) {
 					if (map[x][y] == '1') continue;
-					else if (broken_map_check[x][y] >= 1 && broken_map_check[x][y] != 7777) continue;
+					else if ((broken_map_check[x][y] >= 1 && broken_map_check[x][y] != 7777) || (map_check[x][y] >=1&& map_check[x][y] != 7777)) continue;
 					if (broken_map_check[x][y] == 7777) {
 						flag = 1;
 						short_cut = broken_map_check[p.first][p.second] + 1;
 						break;
 					}
 					broken_map_check[x][y] = broken_map_check[p.first][p.second] + 1;
+					Q.push({ {x,y}, 1 });
 				}
 				else {
 					if (map_check[x][y] >= 1 && map_check[x][y] != 7777) continue;
-					else if (map[x][y] == '1') broken++;
+					else if (map[x][y] == '1') {
+						if (broken_map_check[x][y] != 0) continue;
+						broken_map_check[x][y] = map_check[p.first][p.second] + 1;
+						Q.push({ {x,y}, 1 });
+					}
+					else Q.push({ {x,y},0 });
 					if (map_check[x][y] == 7777) {
 						flag = 1;
 						short_cut = map_check[p.first][p.second] + 1;
@@ -77,7 +89,6 @@ int main() {
 					}
 					map_check[x][y] = map_check[p.first][p.second] + 1;
 				}
-				Q.push({ {x,y},broken });
 			}
 			if (flag == 1) break;
 		}
