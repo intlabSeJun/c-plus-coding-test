ddd6fbfb66c28312b47e18a737bd1179efd0768d
IntlabSeJun <kimsejun5@naver.com>
2023-10-26 16:02:26 +0900

    d

---
+++ 12. 다이나믹 프로그래밍/1 2 3 더하기 (9095).cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int d[20];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	d[1] = 1;
+	d[2] = 2;
+	d[3] = 4;
+	for (int i = 4; i < 11; i++)
+		d[i] = d[i - 1] + d[i - 2] + d[i - 3];
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		cout << d[n] << "\n";
+	}
+}
